c7f07f5dfca3f1514a1325f301415be64043fd05
Steve Aleixo <86091233+SteveAleixo@users.noreply.github.com>
2022-03-07 03:43:44 -0500

    My solution to Problem 1

--- src/Problem001.java
+++ src/Problem001.java
@@ -1,6 +1,7 @@
 public class Problem001 {
     public static int solution() {
         int sumOfTargetNaturals = 0;
+
         for (int i = 1; i < 1000; i++) {
             if (i % 3 == 0 || i % 5 == 0) {
                 sumOfTargetNaturals += i;
